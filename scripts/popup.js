36bc1273d5cca5979b7b705224b38ae1d9f52146
MaximilianPorter <mx.porters@gmail.com>
2023-03-30 15:13:14 -0700

    listing events/stages works with server

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,6 +1,17 @@
 const pre = document.querySelector(`pre`);
 const eventListEls = document.querySelectorAll(`.event-list`);
+const upcomingEventList = document.querySelector(`.events-list-next`);
+const currentEventList = document.querySelector(`.events-list-current`);
+const pastEventList = document.querySelector(`.events-list-past`);
+
 const eventMoreInfoEl = document.querySelector(`.event-more-info`);
+const moreInfo_title = eventMoreInfoEl.querySelector(`.event-title`);
+const moreInfo_image = eventMoreInfoEl
+  .querySelector(`.event-image`)
+  .querySelector("img");
+const moreInfo_stagesList = eventMoreInfoEl.querySelector(`.event-stages`);
+
+const reminderTimeButtons = document.querySelectorAll(`.reminder-time-button`);
 
 // chrome.alarms.create(
 //   "NAME OF EVENT STAGE",
@@ -12,30 +23,248 @@ const eventMoreInfoEl = document.querySelector(`.event-more-info`);
 //   }
 // );
 
-eventListEls.forEach((eventListEl) =>
-  eventListEl.addEventListener(`click`, (event) => {
-    eventMoreInfoEl.classList.add(`event-more-info--active`);
-  })
-);
+const allEvents = await getEvents();
+fillData();
 
-// if there's a click on the pseudo element of the event-more-info element, then close the element
+// click on event (for more info)
+eventListEls.forEach((eventListEl) => {
+  eventListEl.addEventListener(`click`, (event) => {
+    const leagueEvent = event.target.closest(`.event`);
+    if (!leagueEvent) return;
+
+    console.log(event.target);
+    eventMoreInfoEl.classList.add(`event-more-info--active`);
+    addInfoForEvent(leagueEvent.dataset.id);
+  });
+});
+
 eventMoreInfoEl.addEventListener(`click`, (event) => {
+  // close the event-more-info element if the click is on the pseudo element
   if (event.target === eventMoreInfoEl) {
     eventMoreInfoEl.classList.remove(`event-more-info--active`);
+    document
+      .querySelectorAll(`.reminder-section`)
+      .forEach((reminderSection) =>
+        reminderSection.classList.remove(`reminder-section--active`)
+      );
   }
 
+  // if the click is on the remind-me-button, open the reminder-time-buttons
   const remindMeButton = event.target.closest(`.remind-me-button`);
   if (remindMeButton) {
-    remindMeButton.insertAdjacentHTML("afterend", "<p>Reminder set!</p>");
+    const stage = remindMeButton.closest(`.event-stage`);
+    if (!stage) return;
+
+    const reminderSection = stage.querySelector(`.reminder-section`);
+    reminderSection.classList.toggle(`reminder-section--active`);
   }
 });
 
+reminderTimeButtons.forEach((reminderTimeButton) =>
+  reminderTimeButton.addEventListener(`click`, (event) => {
+    console.log(reminderTimeButton.ariaPressed);
+    reminderTimeButton.ariaPressed =
+      reminderTimeButton.ariaPressed === "true" ? "false" : "true";
+  })
+);
+
 document.addEventListener(`click`, (event) => {});
 
-async function getEvents() {
+async function getEvents(mode = undefined, tier = undefined) {
   const response = await fetch(
-    `http://localhost:3000/api/events/upcoming?locale=${navigator.language}`
+    `http://localhost:3000/api/events?mode=${mode}&tier=${tier}`
   );
   const data = await response.json();
-  return data;
+  return data.data;
+}
+
+async function fillData() {
+  const relevantEvents = await getEvents(3, ["S", "A"]);
+  console.log(relevantEvents);
+
+  const upcomingEvents = relevantEvents
+    .filter((event) => Date.now() < new Date(event.startDate))
+    .sort((a, b) => new Date(a.startDate) - new Date(b.startDate));
+
+  addEventToDOM(upcomingEvents[0], upcomingEventList);
+  addEventToDOM(upcomingEvents[1], upcomingEventList);
+  addEventToDOM(upcomingEvents[2], upcomingEventList);
+
+  const pastEvents = relevantEvents
+    .filter((event) => Date.now() > new Date(event.endDate))
+    .sort((a, b) => new Date(b.endDate) - new Date(a.endDate));
+
+  addEventToDOM(pastEvents[0], pastEventList);
+  addEventToDOM(pastEvents[1], pastEventList);
+  addEventToDOM(pastEvents[2], pastEventList);
+
+  const currentEvents = relevantEvents
+    .filter(
+      (event) =>
+        Date.now() > new Date(event.startDate) &&
+        Date.now() < new Date(event.endDate)
+    )
+    .sort((a, b) => new Date(b.startDate) - new Date(a.startDate));
+
+  addEventToDOM(currentEvents[0], currentEventList);
+  addEventToDOM(currentEvents[1], currentEventList);
+  addEventToDOM(currentEvents[2], currentEventList);
+
+  // events.forEach((event) => {
+  //   const { name, startDate, image, stages } = event;
+  //   stages.forEach((stage) => {
+  //     const {
+  //       startDate: stageStartDate,
+  //       endDate: stageEndDate,
+  //       name: stageName,
+  //       liquipedia: stageLiquipediaLink,
+  //     } = stage;
+  //   });
+  // });
+}
+
+// ex. 2023-02-30T00:00:00.000Z -> 1 month ago
+function convertDateToSimplified(date) {
+  const dateObj = new Date(date);
+  const now = new Date();
+  const day = Math.abs(dateObj - now) / (1000 * 60 * 60 * 24);
+  const month = day / 30;
+  const hours = day * 24;
+  const minutes = hours * 60;
+
+  let timeUnit = ["", month];
+  if (month >= 1) {
+    timeUnit[0] = `month`;
+    timeUnit[1] = month;
+  } else if (day >= 1) {
+    timeUnit[0] = `day`;
+    timeUnit[1] = day;
+  } else if (hours >= 1) {
+    timeUnit[0] = `hour`;
+    timeUnit[1] = hours;
+  } else if (minutes >= 1) {
+    timeUnit[0] = `minute`;
+    timeUnit[1] = minutes;
+  } else {
+    return `now`;
+  }
+
+  const value = Math.round(timeUnit[1]);
+
+  if (now < dateObj) {
+    return `in ${value} ${timeUnit[0]}${value > 1 ? `s` : ``}`;
+  } else return `${value} ${timeUnit[0]}${value > 1 ? `s` : ``} ago`;
+}
+// console.log(simplifyDate(new Date(`2023-04-06T18:00:00Z`)));
+function simplifyDate(date, region = "en-US") {
+  const intlFormat = new Intl.DateTimeFormat(region, {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  return intlFormat.format(date);
+}
+
+function addEventToDOM(leagueEvent, eventList) {
+  if (!leagueEvent) return;
+  const { _id, name, startDate, image, stages } = leagueEvent;
+  const markupListItem = `
+      <li class="event" data-id="${_id}">
+        <div class="event-image">
+          <img
+            src="${image}"
+            alt="event image"
+          />
+        </div>
+        <div class="event-date-info">
+          <p class="event-date">${simplifyDate(
+            new Date(startDate),
+            navigator.location
+          )}</p>
+          <p class="event-date--simplified">(${convertDateToSimplified(
+            startDate
+          )})</p>
+        </div>
+        <h3 class="event-title">${name}</h3>
+      </li>
+      `;
+
+  eventList.insertAdjacentHTML(`beforeend`, markupListItem);
+}
+
+function addInfoForEvent(eventId) {
+  const leagueEvent = allEvents.find((event) => event._id === eventId);
+  const { _id, name, startDate, image, stages } = leagueEvent;
+  moreInfo_title.textContent = name;
+  moreInfo_image.src = image;
+  moreInfo_stagesList.innerHTML = ``;
+
+  stages.forEach((stage) => {
+    const {
+      startDate: stageStartDate,
+      endDate: stageEndDate,
+      name: stageName,
+      liquipedia: stageLiquipediaLink,
+    } = stage;
+
+    const stageMarkup = `
+  <div class="event-stage">
+    <h4 class="event-stage-title">
+      ${stageName}
+      <a
+        class="liquipedia-link"
+        href="${stageLiquipediaLink}"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Liquipedia Page</a
+      >
+    </h4>
+    <p class="event-stage-date">&mdash; Start: ${simplifyDate(
+      new Date(stageStartDate),
+      navigator.location
+    )}</p>
+    <p class="event-stage-date">&mdash; End: ${simplifyDate(
+      new Date(stageEndDate),
+      navigator.location
+    )}</p>
+    <button class="remind-button remind-me-button">REMIND ME</button>
+    <div class="reminder-section">
+      <!--  reminder-section--active -->
+      <form action="" class="remind-me-form">
+        <div class="reminder-time-buttons">
+          <button
+            type="button"
+            class="reminder-time-button"
+            aria-pressed="false"
+          >
+            1 Hour Before
+          </button>
+          <button
+            type="button"
+            class="reminder-time-button"
+            aria-pressed="false"
+          >
+            1 Day Before
+          </button>
+          <button
+            type="button"
+            class="reminder-time-button"
+            aria-pressed="false"
+          >
+            1 Week Before
+          </button>
+        </div>
+        <button class="remind-button remind-me-confirm" type="submit">
+          CONFIRM
+        </button>
+      </form>
+    </div>
+  </div>
+  `;
+
+    moreInfo_stagesList.insertAdjacentHTML(`beforeend`, stageMarkup);
+  });
 }
--- server/server.js
+++ server/server.js
@@ -2,13 +2,31 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const cors = require("cors");
+const fs = require("fs");
+
+const testData = require("./testData.json");
 
 app.use(cors());
 
-app.get("/api/events/upcoming?:locale", (req, res) => {
+// mode = integer, (1 = 1v1, 2 = 2v2...)
+// tier = string or array of strings (S, A, B, C...)
+app.get("/api/events?:mode?:tier", (req, res) => {
+  let { mode, tier } = req.query;
+  if (mode === "undefined") mode = undefined;
+  if (tier === "undefined") tier = undefined;
+
+  if (tier?.includes(",")) {
+    tier = tier.split(",");
+  }
+
+  const events = getEventsSortedByDate(testData.events)
+    .filter((event) => (!mode ? true : event.mode == mode))
+    .filter((event) =>
+      !tier ? true : event.tier === tier || tier.includes(event.tier)
+    );
   res.send({
     success: true,
-    data: simplifyDate(new Date(`2023-01-29T06:00:00Z`)),
+    data: events,
   });
 });
 
@@ -16,33 +34,62 @@ server.listen(process.env.PORT || 3000, () => {
   console.log("Server listening on port 3000");
 });
 
-async function getEvents() {
+async function fetchEvents() {
   const intlFormat = new Intl.DateTimeFormat("en-US", {
     year: "numeric",
     month: "2-digit",
     day: "2-digit",
   });
-  const [month, day, year] = intlFormat.format(Date.now()).split("/");
+  let [month, day, year] = intlFormat.format(Date.now()).split("/");
+  month = parseInt(month);
+  day = parseInt(day);
+  year = parseInt(year);
   const today = `${year}-${month}-${day}`;
-  const month2Before = `${year}-${month + 2}-${day}`;
-  const month2After = `${year}-${month - 2}-${day}`;
+  const month2Before = `${year}-${(month + 2).toString().padStart(2, "0")}-${day
+    .toString()
+    .padStart(2, "0")}`;
+  const month2After = `${year}-${(month - 2).toString().padStart(2, "0")}-${day
+    .toString()
+    .padStart(2, "0")}`;
 
   const response = await fetch(
     `https://zsr.octane.gg/events?sort=date%3Aasc&after=${month2After}&before=${month2Before}`
   );
   const data = await response.json();
+  // const data = testData;
+
+  await addMatchesToData(data);
+
   return data;
 }
 
-console.log(simplifyDate(new Date(`2023-01-29T06:00:00Z`)));
+// fetchEvents().then((data) => {
+//   console.log("data fetched");
+//   fs.writeFile("testData.json", JSON.stringify(data), (err) => {
+//     if (err) throw err;
+//     console.log("Data written to file");
+//   });
+// });
 
-function simplifyDate(date, region = "en-US") {
-  const intlFormat = new Intl.DateTimeFormat(region, {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
+function getEventsSortedByDate(events) {
+  return events.sort((a, b) => {
+    const aDate = new Date(a.startDate);
+    const bDate = new Date(b.startDate);
+    return aDate - bDate;
   });
-  return intlFormat.format(date);
+}
+
+async function addMatchesToData(data) {
+  for (const event of data.events) {
+    console.log("fetching matches for event: " + event._id);
+    const matchesResponse = await fetch(
+      `https://zsr.octane.gg/matches?event=${event._id}`
+    );
+    const matchesData = await matchesResponse.json();
+    event.stages.forEach((stage) => {
+      stage.matches = matchesData.matches.filter(
+        (match) => match.stage._id === stage._id
+      );
+    });
+  }
 }
